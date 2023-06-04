7fdb2be1cf2195c9d7113409ace226c826f4cd09
Yong Zheng-Xin <zheng_xin_yong@brown.edu>
2023-06-03 21:48:45 -0400

    Updated index.html from master

---
+++ run.sh
@@ -0,0 +1,21 @@
+git checkout master
+rm -rf _site
+rm -rf index.html
+bundle exec jekyll build
+
+git add .
+git commit -m "update master"
+git push origin master
+
+git checkout gh-pages
+
+# move all the index html files to current folder
+rm -rf *
+git checkout master -- _site
+mv _site/* .
+
+# commit (remember -am instead of -m)
+git commit -am "Updated index.html from master"
+git push origin gh-pages
+
+git checkout master
